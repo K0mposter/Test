4da5d870f522ce80985e2781c276d2d2b9cde09a
Korn'Dan <karret4444@gmail.com>
2021-09-15 19:59:18 +0300

    First commit

---
+++ src/com/company/Plus.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Plus {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("z = x + y: ");
+        System.out.print("x = ");
+        int x = in.nextInt();
+        System.out.print("y = ");
+        int y = in.nextInt();
+        int z = x + y;
+        System.out.printf("z = %d \n", z);
+        in.close();
+    }
+}
